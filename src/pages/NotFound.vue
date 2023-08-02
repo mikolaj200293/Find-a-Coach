3d0ed2a070a2ae6ba8660b14bbd055f95038851b
m.popiolek <m.popiolek@lekseek.com>
2023-08-02 11:13:23 +0200

    Add routes, main layout nad styling

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    FILTER
+  </section>
+  <section>
+    LIST OF COACHES
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
