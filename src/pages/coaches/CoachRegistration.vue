acc95c626f16cb886e4db3ca795e69fc5ba87f21
m.popiolek <m.popiolek@lekseek.com>
2023-08-02 14:26:36 +0200

    Add coach register form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,16 +1,25 @@
 <template>
-  REGISTER
+    <section>
+        <base-card>
+            <h2>Register as a coach</h2>
+            <coach-form></coach-form>
+        </base-card>
+    </section>
 </template>
 
 <script>
+import CoachForm from '@/components/coaches/CoachForm.vue';
+import BaseCard from '@/components/ui/BaseCard.vue';
+
 export default {
-  data() {
-    return {}
-  },
-  methods: {}
-}
+    components: {BaseCard, CoachForm},
+    data() {
+        return {};
+    },
+    methods: {}
+};
 </script>
 
-<style>
+<style scoped>
 
 </style>
